d19dfdc8cb9c2a11f738b6f9a3344b682356398b
Allen Bose <boseallen192@gmail.com>
2025-02-03 18:51:01 +0530

    changed email id to user name!

--- loginManager.php
+++ loginManager.php
@@ -1,23 +1,23 @@
 <?php
 if(isset($_POST['submitBtn'])){
-    if(empty($_POST['emailBox']) || empty($_POST['passwordBox'])){
-	echo "<h3>email or password field is empty!</h3>";
+    if(empty($_POST['userNameBox']) || empty($_POST['passwordBox'])){
+	echo "<h3>userName or password field is empty!</h3>";
     }
     else{
-	$loginEmailID='anakin99@gmail.com';
+	$loginUserName='anakin99';
 	$loginPassword='chancellor66';
 
-	$emailAuth=$_POST['emailBox'] == $loginEmailID ? true:false;
+	$userNameAuth=$_POST['userNameBox'] == $loginUserName ? true:false;
 	$passwordAuth=$_POST['passwordBox'] == $loginPassword ? true:false;
 
-	if(!$emailAuth)
-	    echo "<h3>invalid email id!</h3>";
+	if(!$userNameAuth)
+	    echo "<h3>invalid UserName!</h3>";
 
 	else if(!$passwordAuth)
 	    echo "<h3>invalid password!</h3>";
 
 	else
-	    echo "<h3>User : $loginEmailID logged in Successfully!</h3>";
+	    echo "<h3>User : $loginUserName logged in Successfully!</h3>";
 
     }
 }
